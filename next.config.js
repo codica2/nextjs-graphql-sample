e14d9ceac48870a43d29b2211454193d9cb1848c
Yura Korovko <ykorovko.codica@gmail.com>
2019-05-09 15:32:22 +0300

    [Fix] removed next-css

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const fs = require('fs')
 const path = require('path')
 const withTypescript = require('@zeit/next-typescript')
-const withCss = require('@zeit/next-css')
 const withPlugins = require('next-compose-plugins');
 
 // next.js configuration
